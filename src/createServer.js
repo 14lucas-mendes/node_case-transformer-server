515de5378ef7c55cc914264c654225e98e75bacf
Lucas Mendes <164028753+14lucas-mendes@users.noreply.github.com>
2025-11-16 18:19:06 -0300

    feat: resolution node_case

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,83 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const { createServer: createHttpServer } = require('node:http');
+const { convertToCase } = require('./convertToCase');
+
+/**
+ * Cria e retorna um servidor HTTP que converte textos entre casos.
+ * @returns {import('node:http').Server}
+ */
+function createServer() {
+  const server = createHttpServer((req, res) => {
+    // Sempre responder com JSON
+    res.setHeader('Content-Type', 'application/json');
+
+    const url = req.url || '/';
+    const [path, queryString = ''] = url.split('?');
+
+    // Texto é o path sem a barra inicial
+    const rawText = path.startsWith('/') ? path.slice(1) : path;
+    const text = rawText ? decodeURIComponent(rawText) : '';
+
+    const params = new URLSearchParams(queryString);
+    const toCase = params.get('toCase');
+
+    const supportedCases = new Set([
+      'SNAKE',
+      'KEBAB',
+      'CAMEL',
+      'PASCAL',
+      'UPPER',
+    ]);
+    const errors = [];
+
+    // Validações
+    if (!text) {
+      errors.push({
+        message:
+          'Text to convert is required. Correct request is: ' +
+          '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!toCase) {
+      errors.push({
+        message:
+          '"toCase" query param is required. Correct request is: ' +
+          '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    } else if (!supportedCases.has(toCase)) {
+      errors.push({
+        message:
+          'This case is not supported. Available cases: ' +
+          'SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    // Invoca a lógica de negócio
+    const { originalCase, convertedText } = convertToCase(text, toCase);
+
+    const body = {
+      originalCase,
+      targetCase: toCase,
+      originalText: text,
+      convertedText,
+    };
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.end(JSON.stringify(body));
+  });
+
+  return server;
+}
+
+module.exports = {
+  createServer,
+};
